a0b34b0be62a43a502bc20426c04fe156d78cb4f
DevKab <desarrollo@kabzo.org>
2022-08-01 16:43:38 -0500

    primercommit

---
+++ plugins/youtube-play.js
@@ -0,0 +1,28 @@
+import { youtubeSearch } from '@bochilteam/scraper'
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
+  let vid = (await youtubeSearch(text)).video[0]
+  if (!vid) throw 'Video/Audio Not found'
+  let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
+  const url = 'https://www.youtube.com/watch?v=' + videoId
+  await conn.sendHydrated(m.chat, `
+*Titulo:* ${title}
+*Url:* ${url}
+*Descripción:* ${description}
+*Publicado:* ${publishedTime}
+*Duración:* ${durationH}
+*Views:* ${viewH}
+  `.trim(), author, thumbnail, '', '', null, null, [
+    ['Audio', `${usedPrefix}yta ${url} yes`],
+    ['Video', `${usedPrefix}ytv ${url} yes`]
+  ], m, { asLocation: 1 })
+}
+handler.help = ['play', 'play2'].map(v => v + ' <search>')
+handler.tags = ['downloader']
+handler.command = /^play2?$/i
+
+handler.exp = 0
+handler.limit = false
+
+export default handler
+
